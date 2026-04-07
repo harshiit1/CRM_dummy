5cf84f1b17b62c0f688d729617cd0c3643a37ae0
harshiit1 <bhardwajharshit171@gmail.com>
2026-04-07 11:54:56 +0530

    created Sign UP Authentication API

--- CRM_D.API/CRM_D.BLL/Interfaces/IAuthentication.cs
+++ CRM_D.API/CRM_D.BLL/Interfaces/IAuthentication.cs
@@ -1,14 +1,10 @@
 ﻿using CRM_D.Common.CommonModels;
 using CRM_D.Common.CRMModels.Authentication;
-using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace CRM_D.BLL.Interfaces
 {
     public interface IAuthentication
     {
-        public ResponseModel AddEditUser(UserMaster model);
+        public Task<ResponseModel> AddEditUser(UserMaster model);
     }
 }
