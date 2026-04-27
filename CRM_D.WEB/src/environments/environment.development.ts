c3fd94787ff75bd708969ae7649fcaf64c4aff64
harshiit1 <bhardwajharshit171@gmail.com>
2026-04-27 10:42:12 +0530

    Added Reports and Power BI Reports Section in the Sidebar and added Live backend apiUrl for HTTP requests

--- CRM_D.WEB/src/environments/environment.development.ts
+++ CRM_D.WEB/src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   apiUrl: 'https://localhost:44394/api'
-  // apiUrl: 'https://crm-dummy.onrender.com/api',
+  // apiUrl: 'https://crm-dummy-i23f.onrender.com/api',
 };
